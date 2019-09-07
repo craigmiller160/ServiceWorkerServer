844d832a251323378c9be9bd8de33bb466a0fa5a
Craig Miller <craigmiller160@gmail.com>
2019-09-07 15:51:09 -0400

    Committing progress

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const KoaRouter = require('koa-router');
 const app = new Koa();
 const router = new KoaRouter();
 
+app.use((ctx) => {
+    ctx.set('Access-Control-Allow-Origins', '*');
+    ctx.set('Access-Control-Allow-Methods', 'GET,POST');
+});
+
 router.get('/hello', (ctx) => {
     ctx.body = 'Hello World';
+});
+
+router.all('*', (ctx) => {
+    ctx.body = 'You\'re Not Found';
 });
 
 app.use(router.routes())
