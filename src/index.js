a9be7398fd82623c346863acac419fd5300b6579
Craig Miller <craigmiller160@gmail.com>
2019-09-07 19:21:27 -0400

    Got the auth header working

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Origin', 'http://localhost:9080');
     res.append('Access-Control-Allow-Method', 'GET,POST');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.append('Access-Control-Allow-Credentials', true);
     next();
 });
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World');
+    const headers = Object.entries(req.headers)
+        .filter(([key]) => 'authorization' === key.toLowerCase())
+        .map(([key, value]) => `${key}=${value}`);
+    res.send(`Hello World: ${headers}`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
